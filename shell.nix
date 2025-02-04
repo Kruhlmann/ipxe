bf6679296184154ad34457d4a9a1e4c32053f6b6
Andreas Krühlmann <andreas.kruehlmann@siemens.com>
2025-02-04 18:44:44 +0100

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,15 +1,12 @@
 # Copyright (c) Siemens Mobility A/S 2025, All Rights Reserved - CONFIDENTIAL
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
   buildInputs = [
-    pkgs.ansible
-    pkgs.debootstrap
-    pkgs.dpkg
-    pkgs.rsync
-    pkgs.sshpass
-    pkgs.tigervnc
-    pkgs.xlsx2csv
+    pkgs.gcc
+    pkgs.gnumake
+    pkgs.binutils
+    pkgs.nasm
+    pkgs.gnu-efi
   ];
 }
-
